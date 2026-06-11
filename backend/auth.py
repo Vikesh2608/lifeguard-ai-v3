102ff12ff84bbe11557ff97a2375aacb9218f38c
Vikesh Sagar Bairam <142835187+Vikesh2608@users.noreply.github.com>
2026-06-11 04:03:40 -0400

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -1,18 +1,8 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    deprecated="auto"
-)
-
-def hash_password(password: str):
-    return pwd_context.hash(password)
+def hash_password(password):
+return password
 
 def verify_password(
-    plain_password,
-    hashed_password
+plain_password,
+stored_password
 ):
-    return pwd_context.verify(
-        plain_password,
-        hashed_password
-    )
+return plain_password == stored_password
