3264168d87ad59a061b83dc72b8768dc84c49e8c
Vikesh Sagar Bairam <142835187+Vikesh2608@users.noreply.github.com>
2026-06-11 04:36:30 -0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,32 +1,29 @@
 from sqlalchemy import Column, Integer, String
-
 from database import Base
 
+
 class User(Base):
-**tablename** = "users"
+    __tablename__ = "users"
 
-```
-id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
 
-first_name = Column(String)
-last_name = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
 
-email = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
 
-password = Column(String)
-```
+    password = Column(String)
+
 
 class Wellness(Base):
-**tablename** = "wellness"
+    __tablename__ = "wellness"
 
-```
-id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
 
-email = Column(String)
+    email = Column(String)
 
-mood = Column(String)
+    mood = Column(String)
 
-sleep_hours = Column(Integer)
+    sleep_hours = Column(Integer)
 
-stress_level = Column(Integer)
-```
+    stress_level = Column(Integer)
