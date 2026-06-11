16676f27b29f45215328381263253cc35d5cc866
Vikesh Sagar Bairam <142835187+Vikesh2608@users.noreply.github.com>
2026-06-11 03:11:07 -0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -14,3 +14,13 @@ class User(Base):
     email = Column(String, unique=True, index=True)
 
     password = Column(String)
+
+class WellnessLog(Base):
+    __tablename__ = "wellness_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    sleep_hours = Column(Integer)
+    water_glasses = Column(Integer)
+
+    mood = Column(String)
