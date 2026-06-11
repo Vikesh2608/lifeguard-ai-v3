faa5ac2829b915a85b0e25fefe6b17a33c610de9
Vikesh Sagar Bairam <142835187+Vikesh2608@users.noreply.github.com>
2026-06-11 04:03:09 -0400

    Refactor User and Wellness model attributes

--- backend/models.py
+++ backend/models.py
@@ -2,38 +2,31 @@ from sqlalchemy import Column, Integer, String
 
 from database import Base
 
-
 class User(Base):
-    __tablename__ = "users"
+**tablename** = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+```
+id = Column(Integer, primary_key=True, index=True)
 
-    first_name = Column(String)
-    last_name = Column(String)
+first_name = Column(String)
+last_name = Column(String)
 
-    email = Column(String, unique=True, index=True)
+email = Column(String, unique=True, index=True)
 
-    password = Column(String)
-
-class WellnessLog(Base):
-    __tablename__ = "wellness_logs"
-
-    id = Column(Integer, primary_key=True, index=True)
-
-    sleep_hours = Column(Integer)
-    water_glasses = Column(Integer)
-
-    mood = Column(String)
+password = Column(String)
+```
 
 class Wellness(Base):
-    __tablename__ = "wellness"
+**tablename** = "wellness"
 
-    id = Column(Integer, primary_key=True, index=True)
+```
+id = Column(Integer, primary_key=True, index=True)
 
-    email = Column(String)
+email = Column(String)
 
-    mood = Column(String)
+mood = Column(String)
 
-    sleep_hours = Column(Integer)
+sleep_hours = Column(Integer)
 
-    stress_level = Column(Integer)
+stress_level = Column(Integer)
+```
